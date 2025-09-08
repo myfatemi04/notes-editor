5129e4c14ad61c589f71ed0e9133ecacb6aa4b3f
Michael Fatemi <myfatemi04@gmail.com>
2025-09-08 17:33:36 -0400

    add math support

--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Markdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 type EditorProps = {
   value: string;
@@ -12,14 +15,26 @@ export const Editor: React.FC<EditorProps> = ({
   disabled,
 }) => {
   return (
-    <div className="editor-wrap">
-      <textarea
-        className="textarea"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        spellCheck={false}
-        disabled={disabled}
-      />
+    <div
+      className="editor-wrap"
+      style={{ display: "flex", flexDirection: "row" }}
+    >
+      {/* Textarea */}
+      <div style={{ flex: 1 }}>
+        <textarea
+          className="textarea"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          spellCheck={false}
+          disabled={disabled}
+        />
+      </div>
+      {/* Markdown preview */}
+      <div style={{ flex: 1, borderLeft: 0 }} className="textarea">
+        <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+          {value}
+        </Markdown>
+      </div>
     </div>
   );
 };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import AuthWrapper from "./AuthWrapper";
 import "./index.css";
+import "katex/dist/katex.min.css";
 
 createRoot(document.getElementById("root")!).render(<AuthWrapper />);
